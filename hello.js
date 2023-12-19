f57a71cc40f86cc0a65a8fb5f170b02998f99912
feng qijun <jun1st.feng@gmail.com>
2023-12-19 19:55:05 +0800

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,1 +1,8 @@
-return "Hello";
+$httpClient.get('https://api.my-ip.io/ip', function (error, response, data) {
+  $done({
+    title: 'Current IP Address',
+    content: data,
+    backgroundColor: '#663399',
+    icon: 'network',
+  })
+})
